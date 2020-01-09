f5507d62868691673529f7768312d55f316a98c4
Zhenguang Loh <ZhenGuang_Loh@singaporeair.com.sg>
2020-01-09 12:22:56 +0800

    Updates

---
+++ precache-manifest.82db615af0e84c5ed6c379556340f9c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e191e06f11ea39035d03df78162bd0a6",
+    "url": "/Contact-Manager/index.html"
+  },
+  {
+    "revision": "cb49ca6253de524fa6c3",
+    "url": "/Contact-Manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "88d8f9155b8c11330cc0",
+    "url": "/Contact-Manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "cb49ca6253de524fa6c3",
+    "url": "/Contact-Manager/static/js/2.bb8f0e57.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/Contact-Manager/static/js/2.bb8f0e57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88d8f9155b8c11330cc0",
+    "url": "/Contact-Manager/static/js/main.d44f3fd1.chunk.js"
+  },
+  {
+    "revision": "8811e12751df882b6d93",
+    "url": "/Contact-Manager/static/js/runtime-main.db2c12f0.js"
+  }
+]);
